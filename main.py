dd8596f4c7bea162f63007d1439d3ccbd3f05ec9
daryn <darynnbrown@gmail.com>
2022-06-12 17:52:08 -0500

    completed minesweeper algorithms

--- main.py
+++ main.py
@@ -1,41 +1,48 @@
-from tkinter import * 
-import settings 
+from tkinter import *
+import settings
 import utils
+from cell import Cell
 
 root = Tk()
-#Override the settings of the window
+# Override the settings of the window
 root.configure(bg="black")
-root.geometry(f'{settings.WIDTH}x{settings.HEIGHT}')
+root.geometry(f"{settings.WIDTH}x{settings.HEIGHT}")
 root.title("Minesweeper")
 root.resizable(False, False)
 
 top_frame = Frame(
     root,
-    bg="black", #change later to black
+    bg="black",  
     width=settings.WIDTH,
-    height=utils.height_prct(25) 
-) 
+    height=utils.height_prct(25),
+)
 top_frame.place(x=0, y=0)
 
 left_frame = Frame(
     root,
-    bg='black', # Change later to black
+    bg="black",  
     width=utils.width_prct(25),
-    height=utils.height_prct(75) 
+    height=utils.height_prct(75),
 )
 
 left_frame.place(x=0, y=utils.height_prct(25))
 
 center_frame = Frame(
-    root,
-    bg='black',
-    width=utils.width_prct(75),
-    height=utils.height_prct(75)
+    root, bg="black", width=utils.width_prct(75), height=utils.height_prct(75)
 )
 center_frame.place(
     x=utils.width_prct(25),
     y=utils.height_prct(25),
 )
+
+for x in range(settings.GRID_SIZE):
+    for y in range(settings.GRID_SIZE):
+        c = Cell(x, y)
+        c.create_btn_object(center_frame)
+        c.cell_btn_object.grid(column=x, row=y)
+
+Cell.randomize_mines()
+
 
 # Run the window
 root.mainloop()
